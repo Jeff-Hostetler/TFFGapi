a724e2acfc52ce5a9b8b8252343f43105436df81
Jeff Hostetler <hostetjm@gmail.com>
2015-02-10 19:05:57 -0700

    added shift GET and spec

--- spec/controllers/shifts_controller_spec.rb
+++ spec/controllers/shifts_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ShiftsController, :type => :controller do
 
+  describe "#index" do
+    it "returns JSON of all shifts" do
+      emp = create_employee
+      shift1 = create_shift(emp, 4)
+      shift2 = create_another_shift(emp, 4)
+
+      get :index
+      expect(response.status).to eq(200)
+      response_array = JSON.parse(response.body)
+      expect(response_array.count).to eq(2)
+      expect(response_array[0]["employee_id"]).to eq(emp.id)
+    end
+  end
+
 end
